5054988f6842c66551185657348a5ebf1c90c7ce
hchowdhury3273 <hc2324@nyu.edu>
2019-08-27 14:50:39 -0400

    finished cube

--- AugmentedReality/AugmentedReality/ViewController.swift
+++ AugmentedReality/AugmentedReality/ViewController.swift
@@ -34,16 +34,39 @@ class ViewController: UIViewController {
     }
     
     @IBAction func addCube(_ sender: Any) {
-        let zCoords = randomFloat(min:-2, max:-0.2)
+        //let zCoords = randomFloat(min:-2, max:-0.2)
         
         let cubeNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
-        cubeNode.position = SCNVector3(0,0,zCoords) // in meters
+        //cubeNode.position = SCNVector3(0,0,zCoords) // in meters
+
+        let cc = getCameraCoordinates(sceneView: sceneView)
+        cubeNode.position = SCNVector3(cc.x, cc.y, cc.z)
+        
         sceneView.scene.rootNode.addChildNode(cubeNode)
     
     }
 
 
     @IBAction func addCup(_ sender: Any) {
+    }
+    
+    struct myCameraCoordinates {
+        var x = Float()
+        var y = Float()
+        var z = Float()
+    }
+    
+    func getCameraCoordinates(sceneView: ARSCNView) -> myCameraCoordinates {
+        let cameraTransform = sceneView.session.currentFrame?.camera.transform
+        let cameraCoordinates = MDLTransform(matrix: cameraTransform!)
+        
+        var cc = myCameraCoordinates()
+        cc.x = cameraCoordinates.translation.x
+        cc.y = cameraCoordinates.translation.y
+        cc.z = cameraCoordinates.translation.z
+        
+        return cc
+        
     }
     
 }
