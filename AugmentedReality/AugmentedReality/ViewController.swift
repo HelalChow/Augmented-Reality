ca5cff66748a22be1bea62047524980f083e4084
hchowdhury3273 <hc2324@nyu.edu>
2019-08-27 14:16:22 -0400

    added cube AR

--- AugmentedReality/AugmentedReality/ViewController.swift
+++ AugmentedReality/AugmentedReality/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import ARKit
+import SceneKit
+
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        
+        sceneView.session.run(configuration)
+        
+        
+    }
+    
+    
+    @IBAction func addCube(_ sender: Any) {
+        var cubeNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+        cubeNode.position = SCNVector3(0,0,-0.2) // in meters
+        sceneView.scene.rootNode.addChildNode(cubeNode)
+    
     }
 
 
+    @IBAction func addCup(_ sender: Any) {
+    }
+    
 }
 
